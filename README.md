8b6142dba757f237b265d064ea8dbffa2e49aae0
HoChul Shin <cybrshin@gmail.com>
2012-10-30 19:59:30 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+SimpleLog
+===========================
+
+SimpleLog is for logging messages on mobile web browsers.
+
+This module needs jQuery.
+Also it works with jQuery Mobile.
+
+
---
+++ slog.js
@@ -0,0 +1,36 @@
+/*
+ sLog :
+*/
+
+;( function( $ ){
+
+var slog = function(msg, level) {
+	var header = '<div>',
+		footer = '</div>';
+	if (level) {
+		header = '<div class="' + level + '">';
+	}
+	$('body').append(header + msg + footer);
+}
+
+var slog_jqm = function(msg, level) {
+	if ($.mobile.activePage) {
+		var id = $.mobile.activePage[0].id,
+			header = '<div>',
+			footer = '</div>';
+
+		if (level) {
+			header = '<div class="' + level + '">';
+		}
+
+		$('#'+id).append(header + msg + footer);
+	}
+}
+
+if ($.mobile) { // if jqm is installed
+	window.slog = slog_jqm;
+} else {
+	window.slog = slog;
+}
+
+})( jQuery );
